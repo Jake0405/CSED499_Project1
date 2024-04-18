b80ff91d3d3bee4793a6099c79eb89e25fe75d59
Jake0405 <452shin@gmail.com>
2024-04-19 03:01:20 +0900

    Add: Rotation of Yao Graph

--- calcSP.cpp
+++ calcSP.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 // 그래프의 각 점을 나타내는 구조체
 struct Point {
+    int isValid = 1;
     double x, y; // x와 y 좌표
 
     Point(double _x, double _y) : x(_x), y(_y) {}
@@ -24,8 +25,10 @@ struct Edge {
 
 // Yao 그래프를 나타내는 구조체
 struct YaoGraph {
+    int coneCount;
     vector<Point> points; // 그래프의 모든 점들
     vector<vector<Edge>> adj_list; // 인접 리스트
+    priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> targetPoints;
 
     YaoGraph(int n) : adj_list(n) {} // 점의 수 n으로 그래프 초기화
 };
@@ -72,11 +75,15 @@ unordered_map<int, double> computeShortestPaths(YaoGraph& G, int P, double angle
             int next_point = edge.dest;
             double weight = edge.weight;
 
-            double relative_angle = computeRelativeAngle(G.points[current_point], G.points[next_point]);
+            if (G.points[next_point].isValid == 0)
+                continue;
+
+            double relative_angle = computeRelativeAngle(G.points[P], G.points[next_point]);
             double angle_diff = computeAngleDifference(angle, relative_angle);
 
             // cone ray 내부에 있는 점들만 고려
-            if (angle_diff >= -45.0 && angle_diff <= 45.0) {
+            if (angle_diff >= -180 / G.coneCount && angle_diff < 180 / G.coneCount) {
+                G.targetPoints.push({angle_diff, next_point});
                 if (shortest_paths[current_point] + weight < shortest_paths[next_point]) {
                     shortest_paths[next_point] = shortest_paths[current_point] + weight;
                     pq.push({shortest_paths[next_point], next_point});
@@ -86,4 +93,26 @@ unordered_map<int, double> computeShortestPaths(YaoGraph& G, int P, double angle
     }
 
     return shortest_paths;
+}
+
+void rotateYaoGraph(YaoGraph& G, int P, double rightRayAngle, double rotate) {
+    double current_angle = rightRayAngle;
+    unordered_map<int, double> shortest_paths;
+
+    while (current_angle < rightRayAngle + 360 / G.coneCount) {
+        while (!G.targetPoints.empty()) {
+            auto [angle, target_point] = G.targetPoints.top().second;
+            double angle_diff = computeAngleDifference(angle + rightRayAngle, current_angle);
+
+            if (angle_diff >= rotate / 2)
+                break;
+
+            G.targetPoints.pop();
+            G.points[target_point].isValid = 0;
+
+            shortest_paths = computeShortestPaths(G, P, rightRayAngle);
+        }
+
+        current_angle += rotate;
+    }
 }
